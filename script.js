198f17a8226ae779ab635a8ab98897957ee96821
Terrance Chang <tchang46343@gmail.com>
2019-08-05 07:27:20 -0700

    Updated portfolio with most recent projects. Also fixed description of Quiz App

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-$(".projects").on("click", function(event){
-    event.preventDefault();
-    
-    $("body, html").animate({ 
-        scrollTop: $( $(this).attr('href') ).offset().top 
-    }, 10);
-    
-});
+// $(document).ready(function(){
+//     var scrollLink = $('.scroll');
 
-$(".aboutMe").on("click", function(event){
-    event.preventDefault();
-    
-    $("body, html").animate({ 
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 50);
-    
-});
+//     //smooth scrolling
+//     scrollLink.click(function(e){
+//         e.preventDefault();
+//         $('body,html').animate({
+//             scrollTop: $(this.hash).offset().top
+//         } )
+
+//     })
+// })
+
+// $(".projects").on("click", function(event) {
+//   event.preventDefault();
+
+//   $("body, html").animate(
+//     {
+//       scrollTop: $($(this).attr("href")).offset().top
+//     },
+//     10
+//   );
+// });
+
+// $(".aboutMe").on("click", function(event) {
+//   event.preventDefault();
+
+//   $("body, html").animate(
+//     {
+//       scrollTop: $($(this).attr("href")).offset().top
+//     },
+//     260
+//   );
+// });
