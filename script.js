c0903d5e77bc8d5acfabd94cb83277893987fcc5
Terrance Chang <tchang46343@gmail.com>
2020-01-13 15:39:19 -0800

    Corrected issues base on grader feedback

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-// $(document).ready(function(){
-//     var scrollLink = $('.scroll');
+$(document).ready(function() {
+  var scrollLink = $(".scroll");
 
-//     //smooth scrolling
-//     scrollLink.click(function(e){
-//         e.preventDefault();
-//         $('body,html').animate({
-//             scrollTop: $(this.hash).offset().top
-//         } )
+  //smooth scrolling
+  scrollLink.click(function(e) {
+    e.preventDefault();
+    $("body,html").animate({
+      scrollTop: $(this.hash).offset().top
+    });
+  });
+});
 
-//     })
-// })
+$(".projects").on("click", function(event) {
+  event.preventDefault();
 
-// $(".projects").on("click", function(event) {
-//   event.preventDefault();
+  $("body, html").animate(
+    {
+      scrollTop: $($(this).attr("href")).offset().top
+    },
+    10
+  );
+});
 
-//   $("body, html").animate(
-//     {
-//       scrollTop: $($(this).attr("href")).offset().top
-//     },
-//     10
-//   );
-// });
+$(".aboutMe").on("click", function(event) {
+  event.preventDefault();
 
-// $(".aboutMe").on("click", function(event) {
-//   event.preventDefault();
-
-//   $("body, html").animate(
-//     {
-//       scrollTop: $($(this).attr("href")).offset().top
-//     },
-//     260
-//   );
-// });
+  $("body, html").animate(
+    {
+      scrollTop: $($(this).attr("href")).offset().top
+    },
+    260
+  );
+});
